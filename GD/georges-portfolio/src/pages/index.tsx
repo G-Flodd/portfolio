c18e39d1fe6d874529411e42abe4288394915a14
G-Flodd <georgedodd@hotmail.co.uk>
2023-03-14 10:22:28 +0000

    Updates pre routing

--- GD/georges-portfolio/src/pages/index.tsx
+++ GD/georges-portfolio/src/pages/index.tsx
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube, AiFillGithub } from "react-icons/ai";
+import Image from "next/image";
+import flodd from '../public/flodd.png';
+import Card from './Card.js'
+import projects from './project.json'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,21 +19,71 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
       <main className='bg-blue-300 px-10'>
         <div className='min-h-screen'>
           <section>
             <nav className='py-10 mb-20 flex justify-between'>
-              <h1 className='text-2xl'>George Dodd</h1>
+              <h1 className='text-2xl font-'>The Foolz</h1>
               <ul className='flex items-center'>
                 <li>
-                  <BsFillMoonStarsFill className='cursor-pointer text-3xl' />
-
+                  <AiFillLinkedin className='cursor-pointer text-4xl' />
                 </li>
-                <li><a href="https://www.linkedin.com/in/george-dodd-irwin-b6a11a242/"> LinkedIn </a> </li>
-
+                <li> <a className='bg-gray-600 text-white px-3 py-2 rounded-lg ml-2' href="https:www.linkedin.com/in/george-dodd-irwin-b6a11a242/"> LinkedIn </a> </li>
               </ul>
             </nav>
+
+            <div className=' text-center p-10'>
+              <h2 className='text-5xl py-2 text-gray-600 font-medium'>
+                The Foolz NFT
+              </h2>
+              <h3 className='text-2xl py-2'>Mooner, Rugger and Scem</h3>
+              <p className='text-md py-6 leading-5 text-gray-800 max-w-xl'>
+                Hi Im George,
+
+              </p>
+
+            </div>
+            <div className='text-5xl flex justify-center gap-10 py-4 text-gray-500'>
+              <AiFillTwitterCircle />
+              <AiFillGithub />
+              <AiFillLinkedin />
+            </div>
+            <div className="relative mx-auto rounded-full w-80 h-80 mt-10 overflow-hidden">
+              <Image alt='logo' src={flodd} style={{ objectFit: "cover" }} />
+            </div>
+          </section>
+
+          <section>
+            <div>
+              <h3 className="text-center text-3xl py-2"> Services I offer</h3>
+              <p className="text-center text-md py-2 leading-8 text-gray-900">
+                xxxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxx xxxxxx xxxx  xxxxx x
+                xxxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxx xxxxxx xxxx  xxxxx x
+              </p>
+
+              <p className="text-center text-md py-2 leading-8 text-gray-900">
+                xxxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxx xxxxxx xxxx  xxxxx x
+                xxxxxxxx xxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxx xxxxxxx xxxxxx xxxx  xxxxx x
+              </p>
+            </div>
+
+            <section>
+              <div>
+                <h3 className="text-center text-3xl py-2"> Examples of my work</h3>
+                <p className="text-center text-md py-2 leading-8 text-gray-900">
+
+                </p>
+              </div>
+            </section>
+
+          </section>
+
+          <section>
+       
+
+
+            <Card/>
+
           </section>
 
         </div>
